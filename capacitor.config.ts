ae969929930daa2eb86d0ae3889cf5c7b6f0bb75
Sean Perkins <sean@ionic.io>
2022-01-10 16:44:19 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gh-24545-modal-top',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
